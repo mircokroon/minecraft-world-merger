af10c964a394ab82164f6b44672d7393ee470573
Unknown <mircokroon@users.noreply.github.com>
2019-05-05 15:39:59 +0200

    Improved help messages

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -19,7 +19,7 @@ public class Launcher {
     public static void main(String[] args) throws IOException {
         Namespace ns = getArguments(args);
 
-        RegionFinder f = new RegionFinder(ns.getString("world1"), ns.getString("world2"));
+        RegionFinder f = new RegionFinder(ns.getString("target_world"), ns.getString("source_world"));
         f.mergeWorlds(mergeRules.get(ns.getString("rule")));
     }
 
@@ -33,8 +33,8 @@ public class Launcher {
         parser.addArgument("-r", "--rule")
                 .choices("last-modified", "always", "never").setDefault("last-modified")
                 .help("Set the method used to merge overlapping chunks.");
-        parser.addArgument("world1");
-        parser.addArgument("world2");
+        parser.addArgument("target_world").help("World to for chunks to be merged into. WARNING: chunks in this world may be overwritten.");
+        parser.addArgument("source_world").help("World to be merged from. Chunks in this world will remain unchanged.");
 
         Namespace ns = null;
         try {
